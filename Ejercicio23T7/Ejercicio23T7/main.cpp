43291fd727f4d0e808c865606ba50aa99b44961a
plandevida <plandevida@planes.com>
2013-04-16 22:14:18 +0200

    Cabeceras añadidas

---
+++ Ejercicio23T7/Ejercicio23T7/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Ejercicio23T7
+//
+//  Created by Dani on 16/04/13.
+//  Copyright (c) 2013 Dani. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include "Arbus.h"
+#include "Lista.h"
+
+using namespace std;
+
+void ordenAlfabeticoYRepeticiones(Lista<Lista<string>> &texto);
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
+
+void ordenAlfabeticoYRepeticiones(Lista<Lista<string>> &texto) {
+    
+    
+}
